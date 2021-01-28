7a31e321e0fa79fd0922482d3ddea3901800e7fd
gabibarbarin <77581880+gabibarbarin@users.noreply.github.com>
2021-01-28 10:43:37 -0300

    Update toggle-menu.js

--- Portafolios/js/toggle-menu.js
+++ Portafolios/js/toggle-menu.js
@@ -1,4 +1,4 @@
 $('#menu-icon').click(function(){ //Le agrega la funcion onclick al boton
-    //alert("has hecho click en la imagen");
+    alert("has hecho click en la imagen");
     $('.container-nav').slideToggle(); //le da la aparicion al menu desplegable
 });
