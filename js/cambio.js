a536ed46a0ac843ec9134b9b1f4292a093ce448d
gabibarbarin <77581880+gabibarbarin@users.noreply.github.com>
2021-02-12 19:25:09 -0300

    Update cambio.js

--- js/cambio.js
+++ js/cambio.js
@@ -1,15 +1,15 @@
 function cambio(aux){
     let slide1 = document.getElementById('slide1');
     let elementStyle = window.getComputedStyle(slide1);
-    let opaSlide1 = elementStyle.getPropertyValue('opacity');
+    let opaSlide1 = elementStyle.getPropertyValue('display');
 
     let slide2 = document.getElementById('slide2');
 
-    if(opaSlide1 == 0 && aux == 1){
+    if(opaSlide1 == "none" && aux == 1){
         slide1.style.cssText = "display: block;";
         slide2.style.cssText = "display: none;";
 
-    } else if(opaSlide1 == 1 && aux == 2){
+    } else if(opaSlide1 == "block" && aux == 2){
         slide1.style.cssText = "display: none;";
         slide2.style.cssText = "display: block;";
     }
